e85612625f01b4b546f987059a70155bb2be59a7
dRcmd <76197823+dRcmd@users.noreply.github.com>
2021-09-21 23:54:52 +0300

    donguler-while-foreach

---
+++ donguler-while-foreach/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı giriniz: ");
+            int sayac = 1;
+            int sayi = int.Parse(Console.ReadLine());
+            int toplam = 0;
+            while (sayac<=sayi)
+            {
+                 toplam+=sayac;
+                 sayac++;
+            }
+            Console.WriteLine(toplam/sayac);
+            
+
+            char character = 'a';
+            while (character<'z')
+            {
+                 Console.Write(character);
+                 character++;
+            }
+
+            string[] arabalar = {"BMW","Audi","Mercedes","Renault","Fiat"};
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+
+        }
+    }
+}
