c8f893771e110f24b149941d4753192c333266c8
dRcmd <76197823+dRcmd@users.noreply.github.com>
2021-09-27 21:51:50 +0300

    struct ve enum

---
+++ csharp-enum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace csharp_enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Cuma);
+            Console.WriteLine((int)Gunler.Pazar);
+
+            int sicaklik = 32;
+            if (sicaklik <= (int)HavaDurumu.Normal)
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyin");
+            else if(sicaklik>=(int)HavaDurumu.Sicak)
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün");
+            else if(sicaklik>=(int)HavaDurumu.Normal && sicaklik<(int)HavaDurumu.CokSicak)
+                Console.WriteLine("Hadi dışarı çıkalım");
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi=1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma=23,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal=20,
+        Sicak=25,
+        CokSicak = 30
+
+    }
+}
