387c55baae30f0b7748fb9ff5867e51f5723b2eb
dRcmd <76197823+dRcmd@users.noreply.github.com>
2021-12-24 15:16:54 +0300

    Artık yıl hesaplama

---
+++ Java101/artik-yil-hesaplama/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int yil;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Yıl giriniz : ");
+        yil = in.nextInt();
+
+        if (yil%100==0){
+            if (yil%400 == 0){
+                System.out.println(yil + " bir artık yıldır!");
+            }else{
+                System.out.println(yil + " bir artık yıl değildir!");
+            }
+        }else if(yil%4==0){
+            System.out.println(yil + " bir artık yıldır!");
+        }else{
+            System.out.println(yil + " bir artık yıl değildir!");
+        }
+
+    }
+}
