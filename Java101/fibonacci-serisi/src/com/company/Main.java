54e1d40757d18e8513b3001820f2d9e72cb600c5
dRcmd <76197823+dRcmd@users.noreply.github.com>
2022-01-02 23:21:18 +0300

    fibonacci sayısı

---
+++ Java101/fibonacci-serisi/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Eleman sayısını giriniz : ");
+        int n = in.nextInt();
+
+        int x =0;
+        int y= 1;
+        while(n>=0){
+            y = x+y;
+            System.out.println(x);
+            x = y-x;
+            n--;
+        }
+
+    }
+}
