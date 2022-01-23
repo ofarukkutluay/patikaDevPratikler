abc1c74ea8906b1527c2ca2090f08a65dee54d3a
dRcmd <76197823+dRcmd@users.noreply.github.com>
2022-01-23 16:32:33 +0300

    Final Projesi - MayınTarlası

---
+++ Java101/mayin-tarlasi/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MayinTarlasi mayinTarlasi = new MayinTarlasi(10,10);
+        mayinTarlasi.run();
+    }
+}
