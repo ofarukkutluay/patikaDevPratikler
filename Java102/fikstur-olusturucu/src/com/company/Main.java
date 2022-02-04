c1cc707c385901cee0c6d642ea9b7d27fcaaa40f
dRcmd <76197823+dRcmd@users.noreply.github.com>
2022-02-04 22:24:08 +0300

    Fikstur Oluşturucu

---
+++ Java102/fikstur-olusturucu/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Galatasaray");
+        list.add("Bursaspor");
+        list.add("Fenerbahçe");
+        list.add("Beşiktaş");
+        list.add("Başakşehir");
+        list.add("Trabzonspor");
+        FixtureGenerator fixtureGenerator = new FixtureGenerator();
+        List<List<Match>> all= fixtureGenerator.getFixtures(list);
+        fixtureGenerator.print(all);
+    }
+}
---
+++ Java102/fikstur-olusturucu/src/com/company/Match.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Match {
+
+    String  homeTeam;
+    String  awayTeam;
+
+    public Match(String homeTeam, String awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+}
