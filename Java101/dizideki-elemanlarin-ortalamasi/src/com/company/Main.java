f436babae0ef71e8e860cec98fd60dc76066c129
dRcmd <76197823+dRcmd@users.noreply.github.com>
2022-02-20 17:53:42 +0300

    First Commit

--- Java101/dizideki-elemanlarin-ortalamasi/src/com/company/Main.java
+++ Java101/dizideki-elemanlarin-ortalamasi/src/com/company/Main.java
@@ -1,11 +1,17 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
+        int[] numbers = {6,1, 2, 3, 4, 5};
         int sum = 0;
         for (int i = 0; i < numbers.length; i++) {
             sum += numbers[i];
+        }
+        Arrays.sort(numbers);
+        for (int i:numbers) {
+            System.out.println(i);
         }
 
         System.out.println(sum / numbers.length);
